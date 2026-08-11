eb358bf7c734c47d3be7e7fd6c1a903bd01d08e3
Luana Peixoto <luanapeixotosn@gmail.com>
2026-08-11 19:33:01 -0300

    update

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -5,7 +5,42 @@ module.exports = class TaskController{
         res.render('tasks/create')
     }
 
-    static showTasks(req, res) {
-        res.render('tasks/all')
+    static async createTaskSave(req, res){
+
+        const task = {
+            title: req.body.title,
+            description: req.body.description,
+            done: false
+        }
+
+        await Task.create(task)
+
+        res.redirect('/tasks')
+    }
+    
+    static async removeTask(req, res){
+
+        const id = req.body.id
+
+        await Task.destroy({where: {id:id}})
+
+        res.redirect('/tasks')
+    }
+
+    static async updateTask(req, res){
+
+        const id = req.params.id
+
+        const task = await Task.findOne({where: {id:id}, raw:true})
+
+        res.render('tasks/edit', {task})
+
+    }
+
+    static async showTasks(req, res) {
+
+        const tasks = await Task.findAll({raw: true})
+
+        res.render('tasks/all', {tasks})
     }
 }
--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -4,6 +4,9 @@ const router = express.Router()
 const TaskController = require('../controllers/TaskController')
 
 router.get('/add', TaskController.createTask)
+router.post('/add', TaskController.createTaskSave)
+router.post('/remove', TaskController.removeTask)
+router.get('/edit/:id', TaskController.updateTask)
 router.get('/', TaskController.showTasks)
 
 module.exports = router
